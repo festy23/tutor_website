e689f3e184b686d6d2eaf3bffc0dc0d33750df98
ivan <festteil1406@gmail.com>
2025-07-20 22:52:11 +0300

    minor fixes

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://festy23.github.io/tutor_website/',
   plugins: [react()],
   server: {
     port: 5173,
