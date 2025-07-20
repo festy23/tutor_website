cf5215f034d249b5da3c7df802ca97bfa2cfa5ab
ivan <festteil1406@gmail.com>
2025-07-20 23:22:31 +0300

    minor fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// https://vite.dev/config/
+
 export default defineConfig({
-  base: '/tutor_website/',
+  base: './', 
   plugins: [react()],
-  server: {
-    port: 5173,
-  },
 })
