72825e5c987b8e870c3792ee9881e6c384c659db
ivan <festteil1406@gmail.com>
2025-09-02 11:27:20 +0300

    bug fixes

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/tutor_website_v4/', 
+  base: '/', 
   plugins: [react()],
   resolve: {
     alias: {
